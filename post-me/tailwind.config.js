00782a8089590b663743c728ea6a8e04f265bf3c
mirzavic4869 <avicenamirzam@gmail.com>
2022-07-25 21:18:35 +0700

    menambahkan warna

--- post-me/tailwind.config.js
+++ post-me/tailwind.config.js
@@ -2,6 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
+    colors: {
+      bluepm: "#2b26bf",
+      screen: "#E3E3E3",
+      blackpm: "#323232",
+    },
     extend: {},
   },
   plugins: [],
