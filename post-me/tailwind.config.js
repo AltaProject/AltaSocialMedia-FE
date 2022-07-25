4e2ecd2285cf82ffd1e6827e34a8576ea4d50fd2
mirzavic4869 <avicenamirzam@gmail.com>
2022-07-25 21:23:52 +0700

    menambahkan class name untuk warna

--- post-me/tailwind.config.js
+++ post-me/tailwind.config.js
@@ -3,8 +3,7 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     colors: {
-
-      bluepm: "#2B26BF"
+      bluepm: "#2B26BF",
       screen: "#E3E3E3",
       blackpm: "#323232",
     },
