71b7072261aa2bf623a3005b062cc957d8efc992
mirzavic4869 <avicenamirzam@gmail.com>
2022-07-26 09:06:47 +0700

    test tulisan hello world

--- post-me/src/pages/HomePage.jsx
+++ post-me/src/pages/HomePage.jsx
@@ -1,8 +1,10 @@
 import React from "react";
+import Header from "../Components/Header";
 
 export default function HomePage() {
   return (
     <div>
+      <Header/>
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
     </div>
   );
