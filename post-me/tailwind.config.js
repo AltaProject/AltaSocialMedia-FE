dbd5cef447b323eeddd397b87c74c372e2045bc9
mirzavic4869 <avicenamirzam@gmail.com>
2022-07-26 09:03:55 +0700

    revisi warna

--- post-me/tailwind.config.js
+++ post-me/tailwind.config.js
@@ -2,11 +2,6 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    colors: {
-      bluepm: "#2B26BF",
-      screen: "#E3E3E3",
-      blackpm: "#323232",
-    },
     extend: {},
   },
   plugins: [],
