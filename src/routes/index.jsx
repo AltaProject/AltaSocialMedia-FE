97c1469cd3a5dfa4566d0b084c84d969678e97b5
mirzavic4869 <avicenamirzam@gmail.com>
2022-07-27 16:29:10 +0700

    revisi index

---
+++ src/routes/index.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { BrowserRouter, Routes, Route} from "react-router-dom"
+import HomeAfterLogin from '../pages/HomeAfterLogin'
+import HomeBeforeLogin from '../pages/HomeBeforeLogin'
+import MyProfile from '../pages/MyProfile'
+import PostWithComment from '../pages/PostWithComment'
+ 
+const App= () => {
+  return (
+    <BrowserRouter>
+        <Routes>
+            <Route path="afterlogin" element={<HomeAfterLogin/>} />
+            <Route path="/" element={<HomeBeforeLogin/>} />
+            <Route path="profile" element={<MyProfile/>} />
+            <Route path="comment" element={<PostWithComment/>} />
+            <Route path="*" element={<div>404 Error not Found</div>}/>
+        </Routes> 
+    </BrowserRouter>
+  )
+}
+
+export default App
