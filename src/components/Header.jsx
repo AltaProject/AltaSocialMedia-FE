3169f38b71189104f346da931eb1ca546860233f
mirzavic4869 <avicenamirzam@gmail.com>
2022-07-27 16:27:36 +0700

    membuat halaman home after login

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,12 @@
 import React from 'react'
 import "../styles/index.css"
 import logo from "../image/ori.png"
-import { ButtonSmall } from "./Button"
-
+import { ButtonSmall } from './Button'
 
 const Header = () => {
     return (
-      <div className='h-auto bg-white sticky top-0 flex justify-between'>
-        <img className='p-6 w-40 lg:w-2/12' src={logo} alt='logo'/>
+      <div className='h-18 bg-white sticky top-0 flex justify-between'>
+        <img className='w-36 p-6 lg:w-2/12' src={logo} alt='logo'/>
         <div className='p-3'><ButtonSmall label="Login"/></div>
       </div>
     )
--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./styles/index.css";
-import HomeBeforeLogin from "./pages/HomeBeforeLogin";
-
+import App from "./routes";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <>
-    <HomeBeforeLogin />
+    <App />
   </>
 );
