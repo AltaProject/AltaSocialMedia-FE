53aa5ae7e5957d5119d72e4115755a94a25a069a
argianardi <argianardi1435@gmail.com>
2022-07-27 17:02:06 +0700

    change focus browser

--- src/index.js
+++ src/index.js
@@ -3,12 +3,11 @@ import ReactDOM from "react-dom/client";
 import "./styles/index.css";
 // import HomePage from "./pages/HomePage";
 import MyProfile from "./pages/MyProfile";
-import EditProfile from "./pages/EditProfile";
+// import EditProfile from "./pages/EditProfile";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <>
-    <EditProfile />
-    {/* <MyProfile /> */}
+    <MyProfile />
   </>
 );
