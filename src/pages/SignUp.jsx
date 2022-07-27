b6f97ab657408eab183d53df8148f9f8dfe8e3d9
argianardi <argianardi1435@gmail.com>
2022-07-28 01:00:58 +0700

    memperbaiki tampilan responsive

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,28 +1,46 @@
 import React from "react";
 import "../styles/index.css";
 import Input from "../components/Input";
-export default function HomePage() {
+import { ButtonLarge } from "../components/Button";
+
+export default function SignUp() {
   return (
-    <div className="text-center mt-7">
-      <h1 className="text-3xl font-bold text-center fc-bluepm">Sign Up</h1>
-      <div className="mt-4">
-        <Input placeholder="Name" />
+    <>
+      <div className="bg-screen h-screen">
+        <h2 className="pt-6 pb-4 md:pb-4 text-center text-bluepm text-3xl lg:text-4xl font-bold">
+          Sign Up
+        </h2>
+
+        <form>
+          <div className="mb-5 text-center">
+            <Input type="text" placeholder="Update your name" />
+          </div>
+          <div className="mb-5 text-center">
+            <Input type="text" placeholder="Update your username" />
+          </div>
+          <div className="mb-5 text-center">
+            <Input type="tel" placeholder="Update your number phone" />
+          </div>
+          <div className="mb-5 text-center">
+            <Input type="email" placeholder="Update your email" />
+          </div>
+          <div className="mb-5 text-center">
+            <Input type="text" placeholder="Update your address" />
+          </div>
+          <div className="mb-5 text-center">
+            <Input type="password" placeholder="Update your password" />
+          </div>
+        </form>
+        <div className="text-center">
+          <ButtonLarge label="Create Account" />
+        </div>
+        <p className="text-center text-blackpm m-4 lg:text-xl">
+          Have an account?
+          <button className="ml-1 hover:text-green-900 text-blue-800">
+            Log In
+          </button>
+        </p>
       </div>
-      <div className="mt-4">
-        <Input placeholder="Username" />
-      </div>
-      <div className="mt-4">
-        <Input placeholder="Number Phone" />
-      </div>
-      <div className="mt-4">
-        <Input placeholder="Email" />
-      </div>
-      <div className="mt-4">
-        <Input text="password" placeholder="Password" />
-      </div>
-      <div className="mt-4">
-        <Input placeholder="Address" />
-      </div>
-    </div>
+    </>
   );
 }
