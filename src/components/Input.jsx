20e7bb510137a154052de1f0c6e64236f471b6bc
argianardi <argianardi1435@gmail.com>
2022-07-28 00:59:32 +0700

    memperbaiki tampilan responsive

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -3,7 +3,11 @@ import "../styles/index.css";
 export default function Input(props) {
   return (
     <div>
-      <input className="text-center w-64 bg-white h-9" type={props.text} placeholder={props.placeholder} />
+      <input
+        className="text-center w-10/12 md:w-3/5 h-10  bg-white rounded-md"
+        type={props.type}
+        placeholder={props.placeholder}
+      />
     </div>
   );
 }
